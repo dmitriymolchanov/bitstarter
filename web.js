dacfeaca44c057edb79e66b3bc04b64be6f9c911
dmolchanov <dmitriy.i.molchanov@gmail.com>
2014-03-13 02:42:27 +0000

    Altered web.js to use fs.readFileSync

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
+
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+var content = fs.readFileSync("html.index");
+console.log(buf.toString('utf8', 0, content));
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
